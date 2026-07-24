aa22be86d2f768651e34b7d08a859bf383150414
Mario Cantu <mariocantuo79@gmail.com>
2026-07-24 11:51:02 -0500

    fix: exportar pool desde database.js para memory.js

--- src/services/database.js
+++ src/services/database.js
@@ -2,7 +2,7 @@ import pkg from 'pg';
 import { createHmac, randomBytes } from 'crypto';
 const { Pool } = pkg;
 
-const pool = new Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
